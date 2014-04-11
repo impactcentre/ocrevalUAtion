e76b55f548c387084dbedf6e1dbcb23eb58f6ad0
Tomasz Parkoła <tparkola@gmail.com>
2014-04-11 13:46:18 +0200

    - printing on sysout added

--- src/main/java/eu/digitisation/ngram/PerplexityEvaluatorAssesmentHelper.java
+++ src/main/java/eu/digitisation/ngram/PerplexityEvaluatorAssesmentHelper.java
@@ -19,35 +19,58 @@ public class PerplexityEvaluatorAssesmentHelper {
 		// langModel, OCRFile, contextLengthRange, resultFile
 		File langModelFile = new File(args[0]);
 		File OCRFile = new File(args[1]);
-		
+
 		String OCRText = extractString(OCRFile);
-		
+
 		NgramModel providedModel = new NgramModel(langModelFile);
 
-		ContextLengthRange contextLengthRange = ContextLengthRange.parseContextLengthRange(args[2]);
-		
-		double[][] perplexities = new double[contextLengthRange.getEnd()-contextLengthRange.getStart() + 1][OCRText.length()];
-		
-		LogPerplexityEvaluator logPerplexityEvaluator = new LogPerplexityEvaluator(providedModel);
-		
-		for (int i = contextLengthRange.getStart(); i <=contextLengthRange.getEnd(); i++) {
-			perplexities[i] = logPerplexityEvaluator.calculatePerplexity(OCRText, i);
+		ContextLengthRange contextLengthRange = ContextLengthRange
+				.parseContextLengthRange(args[2]);
+
+		double[][] perplexities = new double[contextLengthRange.getEnd()
+				- contextLengthRange.getStart() + 1][OCRText.length()];
+
+		LogPerplexityEvaluator logPerplexityEvaluator = new LogPerplexityEvaluator(
+				providedModel);
+
+		for (int i = contextLengthRange.getStart(); i <= contextLengthRange
+				.getEnd(); i++) {
+			perplexities[i] = logPerplexityEvaluator.calculatePerplexity(
+					OCRText, i);
 		}
-		
-		//printPerplexities
+
+		printPerplexities(contextLengthRange, OCRText, perplexities);
+	}
+
+	private static void printPerplexities(
+			ContextLengthRange contextLengthRange, String OCRText,
+			double[][] perplexities) {
+		System.out.print("Letter\t");
+		for (int i = contextLengthRange.getStart(); i <= contextLengthRange
+				.getEnd(); i++) {
+			System.out.print("PC" + i + "\t");
+		}
+		System.out.println();
+		for (int j = 0; j < OCRText.length(); j++) {
+			System.out.print(OCRText.charAt(j));
+			for (int i = 0; i < perplexities.length; i++) {
+				System.out.print(perplexities[i][j] + "\t");
+			}
+			System.out.println();
+		}
 	}
 
 	private static String extractString(File OCRFile)
 			throws FileNotFoundException, IOException {
 		BufferedReader reader = new BufferedReader(new FileReader(OCRFile));
-		
+
 		StringBuffer OCRFileText = new StringBuffer();
-		
+
 		String line = null;
-		while ((line = reader.readLine())!=null) {
+		while ((line = reader.readLine()) != null) {
 			OCRFileText.append(line + "\n");
-		}	
-		
+		}
+
 		reader.close();
 		String OCRText = OCRFileText.toString();
 		return OCRText;
